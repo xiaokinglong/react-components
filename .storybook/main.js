0cbce3fdbbffef27660affe1c7fdcff619427f5b
xiaokinglong <xiaokinglong@gmail.com>
2021-11-22 01:19:34 +0800

    11

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,36 +1,51 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    "storybook-dark-mode",
+    // '@storybook/addon-postcss',
+    require.resolve("storybook-addon-grid/preset"),
+    "@pxblue/storybook-rtl-addon/register",
+    "@storybook/addon-viewport",
+    "@storybook/addon-measure",
+    "storybook-addon-themes",
+    "storybook-addon-pseudo-states",
+    "storybook-addon-designs",
+    "storybook-addon-breakpoints",
+    "storybook-color-picker",
+    "storybook-addon-playroom",
+    "storybook-conditional-toolbar-selector",
+    "@storybook/addon-controls",
+    "@storybook/addon-actions",
+    "@geometricpanda/storybook-addon-badges",
+    "@whitespace/storybook-addon-html",
+    "@storybook/addon-storysource",
+    // 'storybook-vscode-component/register',
   ],
-  webpackFinal: async(config) => {
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
         {
-          loader: require.resolve('babel-loader'),
+          loader: require.resolve("babel-loader"),
           options: {
-            presets: [require.resolve('babel-preset-react-app')],
+            presets: [require.resolve("babel-preset-react-app")],
           },
         },
       ],
     });
 
-
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
         {
-          loader: require.resolve('babel-loader'),
+          loader: require.resolve("babel-loader"),
           options: {
-            presets: [require.resolve('babel-preset-react-app')],
+            presets: [require.resolve("babel-preset-react-app")],
           },
         },
       ],
@@ -39,10 +54,10 @@ module.exports = {
     config.module.rules.push({
       test: /\.less$/,
       loaders: [
-        'style-loader',
-        'css-loader',
+        "style-loader",
+        "css-loader",
         {
-          loader: 'less-loader',
+          loader: "less-loader",
           options: {
             lessOptions: {
               javascriptEnabled: true,
@@ -50,9 +65,12 @@ module.exports = {
           },
         },
       ],
-      include: [path.resolve(__dirname, '../src'), /[\\/]node_modules[\\/].*antd/],
+      include: [
+        path.resolve(__dirname, "../src"),
+        /[\\/]node_modules[\\/].*antd/,
+      ],
     });
 
     return config;
-  }
-}
+  },
+};
