2b7850de105d05086d5a0d19fa86bb7c1be16d68
xiaokinglong <xiaokinglong@gmail.com>
2021-11-22 00:52:32 +0800

    tes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +9,50 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async(config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [require.resolve('babel-preset-react-app')],
+          },
+        },
+      ],
+    });
+
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [require.resolve('babel-preset-react-app')],
+          },
+        },
+      ],
+    });
+
+    config.module.rules.push({
+      test: /\.less$/,
+      loaders: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'less-loader',
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+            },
+          },
+        },
+      ],
+      include: [path.resolve(__dirname, '../src'), /[\\/]node_modules[\\/].*antd/],
+    });
+
+    return config;
+  }
 }
